d3c494b2d4997b0ae454ec2695149b3351ac9640
Mateo Lanús Urrets <mateo@MacBook-Air-de-Mateo.local>
2022-02-21 13:14:04 -0300

    prueba

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -124,6 +124,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    return 'fizzbum';
+  }
+  else if (numero % 5 === 0) {
+    return 'buzz';
+  }
+  else if (numero % 3 ===0) {
+    return 'fizz';
+  }
+  else {
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
